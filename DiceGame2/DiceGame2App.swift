38e556dc4356298ee7b8e76610d7145051e9afdb
elina <aelin6277@gmail.com>
2023-01-08 02:49:46 +0100

    Initial Commit

---
+++ DiceGame2/DiceGame2App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGame2App.swift
+//  DiceGame2
+//
+//  Created by Elin A on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct DiceGame2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
